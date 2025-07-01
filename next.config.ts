1b71881ec140675e80e7f3cd1db052313075c6ec
Yohan Chatelain <yohan.chatelain@gmail.com>
2025-07-01 04:11:40 -0400

    Configure Next.js basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/floacon-firebase',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
